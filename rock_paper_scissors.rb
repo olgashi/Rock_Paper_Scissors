82308645102f8feb5dac57cf7ab6cfc9b90ad8d7
Oshi99 <oshiryaeva@protonmail.com>
2018-07-29 18:58:12 -0500

    final changes

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -1,11 +1,11 @@
 
 VALID_CHOICES = %w(rock paper scissors spock lizard)
-RULES = { 'rock' =>  ['scissors', 'lizard'], 'paper' =>  ['spock', 'rock'], 'scissors' =>  ['lizard', 'paper' ], 'spock' => ['rock', 'scissors' ], 'lizard' => ['spock', 'paper'] }
-
-score = { 'Player' => 0, 'Computer'=> 0 }
+RULES = { 'rock' => ['scissors', 'lizard'], 'paper' => ['spock', 'rock'],
+          'scissors' => ['lizard', 'paper'], 'spock' => ['rock', 'scissors'],
+          'lizard' => ['spock', 'paper'] }
 
 def prompt(message)
-  Kernel.puts("=> #{message}")
+  puts("=> #{message}")
 end
 
 def win?(first, second)
@@ -24,8 +24,6 @@ def make_choice(str)
              'spock'
            when 'l'
              'lizard'
-           else
-
            end
 
   choice
@@ -33,9 +31,9 @@ end
 
 def display_winner(winner)
   puts("#{winner} won!")
-end         
+end
 
-def results(player, computer)
+def determine_winner(player, computer)
   if win?(player, computer)
     'Player'
   elsif win?(computer, player)
@@ -47,25 +45,30 @@ def update_score(score, winner)
   score[winner] += 1
 end
 
+def display_score(score)
+  puts"Player score: #{score['Player']}; Computer score: #{score['Computer']}"
+end
+
 def reset_score(score)
-  score['Player'] = 0 
+  score['Player'] = 0
   score['Computer'] = 0
 end
 
-score = { 'Player' => 0, 'Computer'=> 0 }
+score = { 'Player' => 0, 'Computer' => 0 }
 
 loop do
   puts("\n")
   prompt("Welcome to 'ROCK PAPER SCISSORS SPOCK LIZARD' !")
   puts("\n")
   prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-  prompt("Enter 'r' for rock, 'p' for paper, 'sc' for scissors, 'sp' for spock, 'l' for lizzard.  ")
+  prompt("Enter 'r' for rock, 'p' for paper, 'sc' for scissors")
+  puts("\t'sp' for spock, 'l' for lizzard.")
 
   loop do
     choice = ''
+
     loop do
-      puts "Your turn..."
-      
+      puts("Your turn...")
       choice = make_choice(Kernel.gets().chop())
 
       if VALID_CHOICES.include?(choice)
@@ -77,29 +80,30 @@ loop do
 
     computer_choice = VALID_CHOICES.sample
 
-    Kernel.puts("Player chose #{choice}; Computer chose #{computer_choice}")
+    puts("Player chose #{choice}; Computer chose #{computer_choice}")
 
-    winner = results(choice, computer_choice)
-    if winner 
+    winner = determine_winner(choice, computer_choice)
+
+    if winner
       display_winner(winner)
       update_score(score, winner)
     else
       puts "It's a tie!"
     end
 
-    puts "\nPlayer score: #{score['Player']}, computer score: #{score['Computer']}\n\n"
-    if score['Player'] == 5 || score['Computer'] == 5
-      puts "Game is over! Player score: #{score['Player']}, computer score: #{score['Computer']}"
+    display_score(score)
 
+    if score['Player'] == 5 || score['Computer'] == 5
+      puts "Game is over!"
+      display_score(score)
+      reset_score(score)
       break
     end
-      
   end
-  reset_score(score)
-  prompt("Do you want to play again?")
-  answer = Kernel.gets().chomp()
+  prompt("Do you want to play another game?")
+  answer = gets.chomp
 
-  break unless answer.downcase().start_with?('y')
+  break unless answer.downcase.start_with?('y')
 end
 
 prompt("Thank you for playing! Goodbye!")
